20152a9358fe6df2b546ac581f73f749407a2445
Michael Hughes <microneesia79@gmail.com>
2026-06-13 12:50:58 -0700

    Fix v2.8 version sync for TypeScript target

--- src/v28-version-sync.ts
+++ src/v28-version-sync.ts
@@ -1,16 +1,20 @@
 const RELEASE_VERSION = "2.8.0";
 const LEGACY_VERSION = "2.7.0";
 
+function replaceEvery(value: string, searchValue: string, replacement: string) {
+  return value.split(searchValue).join(replacement);
+}
+
 function syncVisibleVersion() {
   const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
   let node = walker.nextNode();
 
   while (node) {
     if (node.nodeValue?.includes(LEGACY_VERSION)) {
-      node.nodeValue = node.nodeValue.replaceAll(LEGACY_VERSION, RELEASE_VERSION);
+      node.nodeValue = replaceEvery(node.nodeValue, LEGACY_VERSION, RELEASE_VERSION);
     }
     if (node.nodeValue?.includes("V2.7.0")) {
-      node.nodeValue = node.nodeValue.replaceAll("V2.7.0", "V2.8.0");
+      node.nodeValue = replaceEvery(node.nodeValue, "V2.7.0", "V2.8.0");
     }
     node = walker.nextNode();
   }
