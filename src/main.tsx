556e62722d9048fe91121219b09891a95db1a9df
Michael Hughes <microneesia79@gmail.com>
2026-06-13 10:51:26 -0700

    Load v2.0 canonical review styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./v19-public-brief.css";
+import "./v20-canonical-review.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
