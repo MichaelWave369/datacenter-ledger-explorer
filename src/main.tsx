da5cb274e95e2325f502e48065acdd33001a11ab
Michael Hughes <microneesia79@gmail.com>
2026-06-13 11:11:53 -0700

    Load v2.3 change approval styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,6 @@ import "./v19-public-brief.css";
 import "./v20-canonical-review.css";
 import "./v21-audit-timeline.css";
 import "./v22-change-review.css";
+import "./v23-change-approval.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
